e7a8c0ca9e63c3009f80350515510e5ac44baf97
Davi Ramos <137097142+terem7@users.noreply.github.com>
2024-07-21 18:40:40 -0300

    .

--- exercicios/ex008/ambiente.js
+++ exercicios/ex008/ambiente.js
@@ -1,10 +1,13 @@
+// var c = 1
+// do {
+//   console.log(`Passo ${c}`)
+//   c++
+// } while (c <= 6)
+
 var c = 1
-while (c <= 6) {
-  console.log('Tudo Bem')
+while (c <= 10) {
+  console.log(`Passo ${c}`)
   c++
 }
-
-
-
 
 // console.log('Tudo Bem')
