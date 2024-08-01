c783efb0da8afa12e2dacb0f1e12e42239d35ae0
Davi Ramos <137097142+terem7@users.noreply.github.com>
2024-08-01 03:44:42 -0300

    .

---
+++ exercicios/ex010/ex01/script.js
@@ -0,0 +1,22 @@
+function contar() {
+  let ini = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    alert('[ERRO] Faltam dados!!!')
+  } else {
+    res.innerHTML = 'Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    
+    if (i < f) {
+      for(let c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1f449}`
+      }
+      res.innerHTML += `\u{1f3c1}`
+    }
+  }
+}
